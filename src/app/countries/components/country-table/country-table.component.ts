0123f27033cabb35ad2cf6eacfe51398fd8079be
jheisson  rojas <jfrojas592@misena.edu.co>
2024-10-29 14:46:19 -0500

    tuberia number

--- src/app/countries/components/country-table/country-table.component.ts
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -5,7 +5,12 @@ import { Country } from '../../interfaces/country';
   selector: 'countries-table',
   templateUrl: './country-table.component.html',
   styles: [
-
+      `img {
+        width: 35px;
+      }`,
+      ` th {
+        padding: 10px;
+      }`
   ]
 })
 export class CountryTableComponent {
